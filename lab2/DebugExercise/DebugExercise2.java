73b6e9453bb9c13e0cecbbd7e3ed917b26e1dcea
Elainasama <1223530014@qq.com>
2023-07-01 01:31:29 +0800

    Lab 2: Debugging

--- lab2/DebugExercise/DebugExercise2.java
+++ lab2/DebugExercise/DebugExercise2.java
@@ -12,7 +12,7 @@ public class DebugExercise2 {
            step out button because you're not going to learn anything. */
         int z = ~(b - a) >> 31;
 
-        int max = b & w | a & z;
+        int max = b & z | a & w;
         return max;
     }
 
@@ -47,6 +47,7 @@ public class DebugExercise2 {
         int[] returnArray = new int[a.length];
         for (int i = 0; i < a.length; i += 1) {
             int biggerValue = max(a[i], b[i]);
+//            System.out.println(biggerValue);
             returnArray[i] = biggerValue;
         }
 
@@ -58,7 +59,7 @@ public class DebugExercise2 {
         int i = 0;
         int sum = 0;
         while (i < x.length) {
-            sum = sum + add(sum, x[i]);
+            sum = add(sum, x[i]);
             i = i + 1;
         }
         return sum;
